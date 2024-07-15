7c8441e259bbd618a5341b14b77ce3118e87fb6c
rhuamanh <rhuamanh@unjbg.edu.pe>
2024-07-14 20:53:33 -0500

    Ejercicio 1 de registros

--- registros1.cpp
+++ registros1.cpp
@@ -1,41 +1,40 @@
-//ejercicio 1
 #include <iostream>
-#include <string>
 using namespace std;
-struct empleado{
-	string nombre;
+
+struct empleados{
+	char nombres [20];
 	char sexo;
-	int sueldo;
-};
+	float sueldo;
+}datos[100];
+
 int main (){
-	int n, mayor=0, menor=10000;
-	cout<<"ingrese la cantidad de trabajadores:";
+	int n, mayor=0, menor=1000000, contador1=0, contador2=0;
+	cout<<"ingrese el numero de empleados:";
 	cin>>n;
-	empleado empleados[n];
-	char sexo[n]; 
-	double sueldo[n];
-	string nombre[n];
 	for (int i=0; i<n; i++){
-		cout<<i+1<<".Ingrese el nombre del empleado:";
-		cin>>nombre[i];
-		cout<<i+1<<".Ingrese el tipo de sexo:";
-		cin>>sexo[i];
-		cout<<i+1<<".Ingrese su sueldo:";
-		cin>>sueldo[i];
-	}
-	for (int i=0; i<mayor; i++){
-		if (sueldo[i]>mayor){
-			mayor=sueldo[i];
+		cout<<"\n--Datos Del empleado"<<i+1<<"--"<<endl;
+		cout<<"\nIngrese el nombre del empleado:";
+		cin>>datos[i].nombres;
+		cout<<"Ingrese el sexo del empleado:";
+		cin>>datos[i].sexo;
+		cout<<"Ingrese el sueldo del empleado:";
+		cin>>datos[i].sueldo;
+		if (datos[i].sueldo>mayor){
+			mayor=datos[i].sueldo;
+			contador1=i;
+		}
+		if (datos[i].sueldo<menor){
+			menor=datos[i].sueldo;
+			contador2=i;
 		}
 	}
-	for (int i=0; i<menor; i++){
-		if (sueldo[i]<menor){
-			menor=sueldo[i];
-		}
-	}
-	cout<<"el sueldo mayor es:"<<endl;
-	cout<<mayor;
-	cout<<"el sueldo menor es:"<<endl;
-	cout<<menor;
+	cout<<"\n***El empleado con el mayor sueldo***"<<endl;
+	cout<<"\n"<<"Nombre:"<<datos[contador1].nombres<<endl;
+	cout<<"Sexo:"<<datos[contador1].sexo<<endl;
+	cout<<"Sueldo:"<<datos[contador1].sueldo<<endl;
+	cout<<"\n***El empleado con el menor sueldo***"<<endl;
+	cout<<"\n"<<"Nombre:"<<datos[contador2].nombres<<endl;
+	cout<<"Sexo:"<<datos[contador2].sexo<<endl;
+	cout<<"Sueldo:"<<datos[contador2].sueldo<<endl;
 	return 0;
 }
